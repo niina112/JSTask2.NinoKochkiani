2ecf40e7680895d46d9cfa2168e4c57789cc4220
niina112 <91963048+niina112@users.noreply.github.com>
2024-11-06 13:16:19 +0400

    created new task

---
+++ script.js
@@ -0,0 +1,14 @@
+let age = -21;
+let ticketPrice;
+
+if (age <= 0 || age > 120) {
+    console.log(`Wrong Age`);
+} else if (age <= 12) {
+    console.log(`price - ${ticketPrice = 5}$`);
+} else if (age <= 17) {
+    console.log(`price - ${ticketPrice = 10}$`);
+} else if (age <= 64) {
+    console.log(`price - ${ticketPrice = 15}$`);
+} else {
+    console.log(`price - ${ticketPrice = 8}$`);
+}
